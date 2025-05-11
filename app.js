330a90a160d7627f072331760ebf92d77ff5018a
Javier Nicolas Martinez <javierformosanm@gmail.com>
2025-05-11 13:26:44 -0300

    Se agrego la function faltante, ya se puede utilizar y se corrigio codigo para hacerlo un poco más eficiente.

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 let indicacion;
 let history = [];
 
-while (indicacion != "2") {
+function convertCurrency(pesos) {
+    return pesos/1300;
+}
+
+while (indicacion !== "2") {
     indicacion = prompt("1.Convertir Pesos -> Dolar (Divisa): \n 2. Salir.");
 
     switch(indicacion){
         case "1":
             let pesos = Number(prompt("Ingrese su monto en pesos para hacer la conversión: "));
-            let usd = pesos/1300;
-            alert("Su monto ingresado es: "+pesos+"ARS - El monto en Dolares es: "+usd+"USD.");
+            let usd = convertCurrency(pesos);
+            alert("Su monto ingresado es: "+pesos+" ARS. \n - El monto en Dolares es: "+usd.toFixed(2)+" USD.");
+            let historial = {
+                tipo: "Divisa",
+                entrada: "Monto ingresado: "+pesos+" ARS.",
+                salida: "Conversión: "+usd.toFixed(2)+" USD.", //Agregado el Tofixed que me sirve para usar solo dos decimales
+                fecha: new Date().toLocaleString() //buscado.
+            };
+
+            history.push(historial)
+            break;
+
+            case "2": 
+            console.log("Historial de todas las conversiones: ");
+            console.table(history);
+            alert("Gracias")
+            break;
+
+            default:
+                alert("El número es invalido.");
+
     }
 }
